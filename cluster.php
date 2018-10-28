77c8c6e84521ac1d8a204041e13bf83b518c458f
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-28 21:51:46 +0000

    no message

--- cluster.php
+++ cluster.php
@@ -1,10 +1,9 @@
 <?php
-
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
-// ini_set('error_reporting', E_ALL); 
-
 // MCP Controller Cluster - Cluster Management console Scripts
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL); 
 
 $api_url = 'http://dashboard.miningcontrolpanel.com';
 
