f9d08035a099c1ee3bbea89ca3b2e381e57c2a66
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-28 21:36:52 +0000

    no message

--- node_scanner.sh
+++ node_scanner.sh
@@ -17,7 +17,7 @@ touch /mcp_cluster/node_ip_address.txt
 SITE_API_KEY="$1"
 
 JSON_DATA=$(curl -sS "http://dashboard.miningcontrolpanel.com/api/?key=$SITE_API_KEY&c=site_ip_ranges")
-#echo $JSON_DATA
+echo $JSON_DATA
 
 SITE_NAME=`echo "$JSON_DATA" | jq -r .site.name`
 
