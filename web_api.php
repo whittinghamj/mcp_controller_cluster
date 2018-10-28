cff3065ab91c0f743fc122a79f6b6f6fd2be6fc6
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-28 23:27:16 +0000

    no message

--- web_api.php
+++ web_api.php
@@ -19,8 +19,66 @@ function json_output($data)
 	die();
 }
 
-// get cpu load
-$cpu_load = sys_getloadavg();
+// get number of system cores
+function system_cores()
+{
+    $cmd = "uname";
+    $OS = strtolower(trim(shell_exec($cmd)));
+ 
+    switch($OS) {
+       case('linux'):
+          $cmd = "cat /proc/cpuinfo | grep processor | wc -l";
+          break;
+       case('freebsd'):
+          $cmd = "sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2";
+          break;
+       default:
+          unset($cmd);
+    }
+ 
+    if ($cmd != '') {
+       $cpuCoreNo = intval(trim(shell_exec($cmd)));
+    }
+    
+    return empty($cpuCoreNo) ? 1 : $cpuCoreNo;
+}
+
+// get system load
+function system_load($coreCount = 2, $interval = 1)
+{
+	$rs = sys_getloadavg();
+	$interval = $interval >= 1 && 3 <= $interval ? $interval : 1;
+	$load = $rs[$interval];
+	return round(($load * 100) / $coreCount,2);
+}
+
+// get memory usage
+function system_memory_usage()
+{
+	$free = shell_exec('free');
+	$free = (string)trim($free);
+	$free_arr = explode("\n", $free);
+	$mem = explode(" ", $free_arr[1]);
+	$mem = array_filter($mem);
+	$mem = array_merge($mem);
+	$memory_usage = $mem[2] / $mem[1] * 100;
+ 
+	return $memory_usage;
+}
+
+// system uptime
+function system_uptime()
+{
+	$uptime = floor(preg_replace ('/\.[0-9]+/', '', file_get_contents('/proc/uptime')) / 86400);
+	
+	return $uptime;
+}
+
+// get system stats
+$cpu_cores 				= system_cores();
+$cpu_load 				= system_load($cpu_cores, 1);
+$memory_usage 			= system_memory_usage();
+$uptime 				= system_uptime();
 
 // build $cluster vars
 $cluster['version']								= '1.0.0.0';
@@ -32,5 +90,8 @@ if($hostname == 'cluster-master')
     $cluster['machine']['type'] 				= 'slave';
 }
 $cluster['machine']['ip_address'] 				= exec('sh /mcp_cluster/lan_ip.sh');
-$cluster['machine']['cpu_usage']				= $cpu_load[0];
+$cluster['machine']['cpu_cores']				= $cpu_cores;
+$cluster['machine']['cpu_load']					= $cpu_load;
+$cluster['machine']['memory_usage']				= $memory_usage;
+$cluster['machine']['uptime']					= $uptime;
 json_output($cluster);
