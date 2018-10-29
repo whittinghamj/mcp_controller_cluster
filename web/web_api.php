0fe5000291dfa0efa0b47f9c88b05addc2f37b89
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-29 19:49:11 +0000

    no message

--- web/web_api.php
+++ web/web_api.php
@@ -105,15 +105,19 @@ function web_cluster_details_table()
 
 function test()
 {
-	$node_json 			= file('/mcp_cluster/nodes.txt');
+	$stat1 = file('/proc/stat'); 
+	sleep(1); 
+	$stat2 = file('/proc/stat'); 
+	$info1 = explode(" ", preg_replace("!cpu +!", "", $stat1[0])); 
+	$info2 = explode(" ", preg_replace("!cpu +!", "", $stat2[0])); 
+	$dif = array(); 
+	$dif['user'] = $info2[0] - $info1[0]; 
+	$dif['nice'] = $info2[1] - $info1[1]; 
+	$dif['sys'] = $info2[2] - $info1[2]; 
+	$dif['idle'] = $info2[3] - $info1[3]; 
+	$total = array_sum($dif); 
+	$cpu = array(); 
+	foreach($dif as $x=>$y) $cpu[$x] = round($y / $total * 100, 1);
 
-	var_dump($node_json);
-
-	echo '<hr>';
-	$node_data			= json_decode($node_json, true);
-
-	echo '<pre>';
-	print_r($node_data);
-
-	
+	json_output($cpu);
 }
