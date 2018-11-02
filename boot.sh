8abdb20d1b9c0c0e3d28f03af58fe9ab21145e83
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-02 22:40:25 +0000

    no message

--- boot.sh
+++ boot.sh
@@ -28,7 +28,8 @@ sudo sh /mcp_cluster/update.sh
 shellinaboxd -t -b -p 8888 --no-beep \-s '/htop_app/:nobody:nogroup:/:htop -d 10' --css /root/shellinabox/white-on-black.css
 
 ## stop services that are not needed on a slave
-if  if [[ "$HOSTNAME" == 'cluster-node' ]]; then
+if [[ "$HOSTNAME" == 'cluster-node' ]]
+then
 	# stop mysql server
 	echo 'Booting MCP Cluster Slave'
 	sudo /etc/init.d/mysql stop > /dev/null 2>&1
