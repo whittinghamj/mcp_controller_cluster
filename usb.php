33fbb19e7aca782e601dc7a5e84002c5f604d5f1
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-02 18:25:49 +0000

    no message

--- usb.php
+++ usb.php
@@ -17,18 +17,35 @@ if($task == "install_config_file")
 	$mount_point = '/mnt/mcp_key';
 	$config_file = 'global_vars.php';
 
+	// sanity checks
+	if(!file_exists('/etc/mcp'))
+	{
+		exec('sudo mkdir /etc/mcp');
+	}
+	if(!file_exists('/mnt/mcp_key'))
+	{
+		exec('sudo mkdir /mnt/mcp_key');
+	}
+
 	// try and mount the usb key
 	if(file_exists($usb_key))
 	{
 		// try and mount the usb key
+		console_output("Attempting to mount MCP USB key.");
+
 		exec("sudo mount ".$usb_key." ".$mount_point);
+	}else{
+		console_output("MCP USB Key not found.");
 	}
 
 	// see if the config file exists
 	if(file_exists($mount_point.'/'.$config_file))
 	{
 		// looks like the file is there, lets copy it
+		console_output("Copying MCP Cluster confif file.");
 		exec("sudo cp ".$mount_point."/".$config_file." /etc/mcp/");
+	}else{
+		console_output("MCP Cluster config file not found on USB key.");
 	}
 
 	console_output("Copied new config file for MCP.");
