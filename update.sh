00b1d75db245f4a0ad9cb8a6199999c16c9aca3d
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-02 22:33:57 +0000

    no message

--- update.sh
+++ update.sh
@@ -2,14 +2,9 @@
 
 ## MCP Cluster - Update Script (git pull)
 
-mv /mcp_cluster/global_vars.php /mcp_cluster/global_vars.tmp
-
 cd /mcp_cluster && git --git-dir=/mcp_cluster/.git pull origin master
 
 crontab /mcp_cluster/crontab.txt
 
-mv /mcp_cluster/global_vars.tmp /mcp_cluster/global_vars.php
-
-chmod 777 /mcp_cluster/global_vars.php
-
 cp -R /mcp_cluster/web/* /var/www/html/
+
