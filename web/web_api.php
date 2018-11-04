58d4b4e4a223840d63f988589df0261476f0eb65
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-04 20:17:55 +0000

    no message

--- web/web_api.php
+++ web/web_api.php
@@ -58,54 +58,9 @@ function node_info()
 	global $db;
 	
 	// get system stats
-	$data['cpu_type'] 				= exec("sed -n 's/^model name[ \t]*: *//p' /proc/cpuinfo | head -n 1");
-	$data['cpu_cores'] 				= system_cores();
-	$data['cpu_load'] 				= cpu_load($data['cpu_cores']);
-	$data['cpu_temp']				= number_format(exec("cat /sys/class/thermal/thermal_zone0/temp") / 1000, 2);
-	$data['memory_usage'] 			= system_memory_usage();
-	$data['uptime'] 				= system_uptime();
-
-    if(file_exists('/sys/firmware/devicetree/base/model'))
-    {
-        $data['hardware'] 				= exec("cat /sys/firmware/devicetree/base/model");
-    }else{
-    	$data['hardware'] 				= 'Raspberry Pi x86 Server';
-    }
-
-	$data['ip_address'] 			= exec("sh /mcp_cluster/lan_ip.sh");
-	$data['mac_address']			= strtoupper(exec("cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address"));
-	$data['hostname']               = exec('cat /etc/hostname');
-
-	if($data['hostname'] == 'cluster-master')
-	{
-		$data['node_type'] = 'master';
-	}else{
-		$data['node_type'] = 'slave';
-	}
-
-	$does_node_exist		= does_node_exist($data['mac_address']);
-	if($does_node_exist == 0)
-	{
-		// cant find this node, lets get it added
-		$result = $db->exec("INSERT INTO `nodes` 
-			(`updated`,`type`, `uptime`, `ip_address`, `mac_address`, `hardware`, `cpu_type`, `cpu_load`, `cpu_cores`, `cpu_temp`, `memory_usage`)
-			VALUE
-			('".time()."','".$data['node_type']."', '".$data['uptime']."', '".$data['ip_address']."', '".$data['mac_address']."', '".$data['hardware']."', '".$data['cpu_type']."','".$data['cpu_load']."', '".$data['cpu_cores']."', '".$data['cpu_temp']."', '".$data['memory_usage']."' )");
-		$data['node_id'] = $db->lastInsertId();	
-	}else{
-		// existing node, update details
-		$bits = get_node_details($data['mac_address']);
-
-		$data['node_id'] = $bits['id'];
-
-		$result = $db->exec("UPDATE `nodes` SET `updated` = '".time()."' WHERE `id` = '".$data['node_id']."' ");
-		$result = $db->exec("UPDATE `nodes` SET `type` = '".$data['node_type']."' WHERE `id` = '".$data['node_id']."' ");
-		$result = $db->exec("UPDATE `nodes` SET `uptime` = '".$data['uptime']."' WHERE `id` = '".$data['node_id']."' ");
-		$result = $db->exec("UPDATE `nodes` SET `ip_address` = '".$data['ip_address']."' WHERE `id` = '".$data['node_id']."' ");
-		$result = $db->exec("UPDATE `nodes` SET `cpu_load` = '".$data['cpu_load']."' WHERE `id` = '".$data['node_id']."' ");
-		$result = $db->exec("UPDATE `nodes` SET `cpu_temp` = '".$data['cpu_temp']."' WHERE `id` = '".$data['node_id']."' ");
-		$result = $db->exec("UPDATE `nodes` SET `memory_usage` = '".$data['memory_usage']."' WHERE `id` = '".$data['node_id']."' ");
-	}
+	$mac_address			= strtoupper(exec("cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address"));
+	
+	$data = get_node_details($mac_address)
 
 	json_output($data);
 }
