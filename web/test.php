2f95085b26e40d00bcee2aaba1403ca44f671759
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 01:24:42 +0000

    no message

--- web/test.php
+++ web/test.php
@@ -8,9 +8,19 @@ $graph->addNode('Router', array('URL'   => 'http://link1', 'label' => 'Router', 
 
 $graph->addNode('Master', array('URL'   => 'http://link1', 'label' => 'Master', 'shape' => 'box'));
 
-// $graph->addEdge(array('Node1' => 'Node2'), array('label' => 'Edge Label'));
+$graph->addNode('Slave 1', array('URL'   => 'http://link1', 'label' => 'Master', 'shape' => 'box'));
+$graph->addNode('Slave 2', array('URL'   => 'http://link1', 'label' => 'Master', 'shape' => 'box'));
+$graph->addNode('Slave 3', array('URL'   => 'http://link1', 'label' => 'Master', 'shape' => 'box'));
+$graph->addNode('Slave 4', array('URL'   => 'http://link1', 'label' => 'Master', 'shape' => 'box'));
 
-$graph->addEdge(array('Router' => 'Master'), array('color' => 'green'));
+$graph->addEdge(array('Master' => 'Router'), array('color' => 'green'));
+
+$graph->addEdge(array('Master' => 'Router'), array('color' => 'green'));
+
+$graph->addEdge(array('Slave 1' => 'Master'), array('color' => 'green'));
+$graph->addEdge(array('Slave 2' => 'Master'), array('color' => 'green'));
+$graph->addEdge(array('Slave 3' => 'Master'), array('color' => 'green'));
+$graph->addEdge(array('Slave 4' => 'Master'), array('color' => 'green'));
 
 $graph->image();
 
