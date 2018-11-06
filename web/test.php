36abbd8388b961efc6aa8696a415b191b6fd7e1f
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 01:45:02 +0000

    no message

--- web/test.php
+++ web/test.php
@@ -4,21 +4,20 @@ require_once 'Image/GraphViz.php';
 
 $graph = new Image_GraphViz();
 
-$graph->addNode('Router', array('URL'   => 'http://link1', 'label' => 'Router', 'shape' => 'box'));
+$graph->addNode('Router', array('label' => 'Router', 'shape' => 'box'));
 
-$graph->addNode('Master', array('URL'   => 'http://link1', 'label' => 'Master', 'shape' => 'box'));
+$graph->addNode('Master', array('label' => 'Master', 'shape' => 'box'));
 
-$graph->addNode('Slave 1', array('URL'   => 'http://link1', 'label' => 'Slave 1', 'shape' => 'box'));
-$graph->addNode('Slave 2', array('URL'   => 'http://link1', 'label' => 'Slave 2', 'shape' => 'box'));
-$graph->addNode('Slave 3', array('URL'   => 'http://link1', 'label' => 'Slave 3', 'shape' => 'box'));
-$graph->addNode('Slave 4', array('URL'   => 'http://link1', 'label' => 'Slave 4', 'shape' => 'box', 'color' => 'black'));
+$graph->addNode('Slave 1', array('label' => 'Slave 1', 'shape' => 'box', 'fillcolor' => 'green', 'style' => 'filled')));
+$graph->addNode('Slave 2', array('label' => 'Slave 2', 'shape' => 'box', 'color' => 'green')));
+$graph->addNode('Slave 3', array('label' => 'Slave 3', 'shape' => 'box', 'color' => 'green')));
+$graph->addNode('Slave 4', array('label' => 'Slave 4', 'shape' => 'box', 'color' => 'green'));
 
 
 $graph->addEdge(array('Slave 1' => 'Master'), array('color' => 'blue'));
 $graph->addEdge(array('Slave 2' => 'Master'), array('color' => 'blue'));
 $graph->addEdge(array('Slave 3' => 'Master'), array('color' => 'blue'));
 $graph->addEdge(array('Slave 4' => 'Master'), array('color' => 'blue'));
-
 $graph->addEdge(array('Master' => 'Router'), array('color' => 'green'));
 
 $graph->image();
