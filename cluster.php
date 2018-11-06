fb5d098c12d3390a7bcc326bf43d86e33842f128
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 04:45:00 +0000

    no message

--- cluster.php
+++ cluster.php
@@ -94,7 +94,7 @@ if($task == "node_checkin")
         $insert = $db->exec("INSERT INTO `nodes` 
             (`updated`,`type`, `uptime`, `ip_address`, `ip_address_wan`, `mac_address`, `hardware`, `cpu_type`, `cpu_load`, `cpu_cores`, `cpu_temp`, `memory_usage`)
             VALUE
-            ('".time()."','".$data['node_type']."', '".$data['uptime']."', '".$data['ip_address']."', '".$data['ip_address_wan']."', ".$data['mac_address']."', '".$data['hardware']."', '".$data['cpu_type']."','".$data['cpu_load']."', '".$data['cpu_cores']."', '".$data['cpu_temp']."', '".$data['memory_usage']."' )");
+            ('".time()."','".$data['node_type']."', '".$data['uptime']."', '".$data['ip_address']."', '".$data['ip_address_wan']."', '".$data['mac_address']."', '".$data['hardware']."', '".$data['cpu_type']."','".$data['cpu_load']."', '".$data['cpu_cores']."', '".$data['cpu_temp']."', '".$data['memory_usage']."' )");
         
         if (!$insert) {
             echo "\nPDO::errorInfo():\n";
