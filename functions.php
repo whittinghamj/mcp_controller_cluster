f79f626ac85f18984c3853e0e7cb1d99d615ce7a
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 00:28:14 +0000

    no message

--- functions.php
+++ functions.php
@@ -435,11 +435,10 @@ function get_system_stats()
         $data['hardware']           = 'Raspberry Pi x86 Server';
     }
 
-    $data['hardware']               = str_replace('Rev 1', '', $data['hardware']);
-    $data['hardware']               = str_replace('Rev 1.0', '', $data['hardware']);
-    $data['hardware']               = str_replace('Rev 1.1', '', $data['hardware']);
-    $data['hardware']               = str_replace('Rev 1.2', '', $data['hardware']);
-    $data['hardware']               = str_replace('Rev 1.3', '', $data['hardware']);
+    $data['hardware']               = str_replace(' Rev 1.0', '', $data['hardware']);
+    $data['hardware']               = str_replace(' Rev 1.1', '', $data['hardware']);
+    $data['hardware']               = str_replace(' Rev 1.2', '', $data['hardware']);
+    $data['hardware']               = str_replace(' Rev 1.3', '', $data['hardware']);
 
     $data['ip_address']             = exec("sh /mcp_cluster/lan_ip.sh");
     $data['ip_address_wan']         = exec("dig +short myip.opendns.com @resolver1.opendns.com");
