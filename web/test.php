90ad495f6aa85682597680b33219d29fd3145c2e
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 01:23:08 +0000

    no message

--- web/test.php
+++ web/test.php
@@ -1,100 +1,18 @@
 <?php
 
- require_once 'Image/GraphViz.php';
+require_once 'Image/GraphViz.php';
 
- 
+$graph = new Image_GraphViz();
 
- $graph = new Image_GraphViz();
+$graph->addNode('Router', array('URL'   => 'http://link1', 'label' => 'Router', 'shape' => 'box'));
 
- 
+$graph->addNode('Master', array('URL'   => 'http://link1', 'label' => 'Master', 'shape' => 'box'));
 
- $graph->addNode(
+// $graph->addEdge(array('Node1' => 'Node2'), array('label' => 'Edge Label'));
 
-   'Node1',
+$graph->addEdge(array('Router' => 'Master'), array('color' => 'green'));
 
-   array(
+$graph->image();
 
-     'URL'   => 'http://link1',
-
-     'label' => 'This is a label',
-
-     'shape' => 'box'
-
-   )
-
- );
-
- 
-
- $graph->addNode(
-
-   'Node2',
-
-   array(
-
-     'URL'      => 'http://link2',
-
-     'fontsize' => '14'
-
-   )
-
- );
-
- 
-
- $graph->addNode(
-
-   'Node3',
-
-   array(
-
-     'URL'      => 'http://link3',
-
-     'fontsize' => '20'
-
-   )
-
- );
-
- 
-
- $graph->addEdge(
-
-   array(
-
-     'Node1' => 'Node2'
-
-   ),
-
-   array(
-
-     'label' => 'Edge Label'
-
-   )
-
- );
-
- 
-
- $graph->addEdge(
-
-   array(
-
-     'Node1' => 'Node2'
-
-   ),
-
-   array(
-
-     'color' => 'red'
-
-   )
-
- );
-
- 
-
- $graph->image();
-
- ?>
+?>
 
