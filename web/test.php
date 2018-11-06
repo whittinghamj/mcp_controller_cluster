3c8b2c8d766e9dafbe19d564f623173c6d96db21
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 01:45:53 +0000

    no message

--- web/test.php
+++ web/test.php
@@ -8,9 +8,9 @@ $graph->addNode('Router', array('label' => 'Router', 'shape' => 'box'));
 
 $graph->addNode('Master', array('label' => 'Master', 'shape' => 'box'));
 
-$graph->addNode('Slave 1', array('label' => 'Slave 1', 'shape' => 'box', 'fillcolor' => 'green', 'style' => 'filled')));
-$graph->addNode('Slave 2', array('label' => 'Slave 2', 'shape' => 'box', 'color' => 'green')));
-$graph->addNode('Slave 3', array('label' => 'Slave 3', 'shape' => 'box', 'color' => 'green')));
+$graph->addNode('Slave 1', array('label' => 'Slave 1', 'shape' => 'box', 'fillcolor' => 'green', 'style' => 'filled'));
+$graph->addNode('Slave 2', array('label' => 'Slave 2', 'shape' => 'box', 'color' => 'green'));
+$graph->addNode('Slave 3', array('label' => 'Slave 3', 'shape' => 'box', 'color' => 'green'));
 $graph->addNode('Slave 4', array('label' => 'Slave 4', 'shape' => 'box', 'color' => 'green'));
 
 
