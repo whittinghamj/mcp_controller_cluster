2807c859fac621e5d19fbe8f4093b5a889395976
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 01:11:45 +0000

    no message

--- web/test.php
+++ web/test.php
@@ -2,7 +2,10 @@
 require_once 'Image/GraphViz.php';
 
 $gv = new Image_GraphViz();
-$gv->addEdge(array('wake up'        => 'visit bathroom'));
-$gv->addEdge(array('visit bathroom' => 'make coffee'));
+$gv->addEdge(array('Router'        => 'MCP Master'));
+$gv->addEdge(array('MCP Master' => 'MCP Slave 1'));
+$gv->addEdge(array('MCP Master' => 'MCP Slave 2'));
+$gv->addEdge(array('MCP Master' => 'MCP Slave 3'));
+$gv->addEdge(array('MCP Master' => 'MCP Slave 4'));
 $gv->image();
 ?>
