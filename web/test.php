93e51f268eb112302a6e5ac4f3adf4c0059ad726
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 03:52:22 +0000

    no message

--- web/test.php
+++ web/test.php
@@ -2,22 +2,34 @@
 
 require_once 'Image/GraphViz.php';
 
+include('/mcp_cluster/db.php');
+include('/mcp_cluster/functions.php');
+
 $graph = new Image_GraphViz();
 
-$graph->addNode('Slave 1', array('label' => 'Slave 1', 'shape' => 'box', 'fillcolor' => 'darkseagreen1', 'style' => 'filled'));
-$graph->addNode('Slave 2', array('label' => 'Slave 2', 'shape' => 'box', 'fillcolor' => 'darkseagreen1', 'style' => 'filled'));
-$graph->addNode('Slave 3', array('label' => 'Slave 3', 'shape' => 'box', 'fillcolor' => 'darkseagreen1', 'style' => 'filled'));
-$graph->addNode('Slave 4', array('label' => 'Slave 4', 'shape' => 'box', 'fillcolor' => 'darkseagreen1', 'style' => 'filled'));
+$nodes 		= get_nodes();
+
+foreach($nodes as $node)
+{
+	if($node['type'] == 'slave')
+	{
+		$graph->addNode('Slave '.$node['id'], array('label' => 'Slave '.$node['id'], 'shape' => 'box', 'fillcolor' => ($node['status']=='online'? 'darkseagreen1':'coral1'), 'style' => 'filled'));
+	}
+}
 
 $graph->addNode('Master', array('label' => 'Master', 'shape' => 'box', 'fillcolor' => 'darkseagreen1', 'style' => 'filled'));
 
 $graph->addNode('Router', array('label' => 'Router', 'shape' => 'box'));
 
 
-$graph->addEdge(array('Slave 1' => 'Master'), array('color' => 'blue'));
-$graph->addEdge(array('Slave 2' => 'Master'), array('color' => 'blue'));
-$graph->addEdge(array('Slave 3' => 'Master'), array('color' => 'blue'));
-$graph->addEdge(array('Slave 4' => 'Master'), array('color' => 'blue'));
+foreach($nodes as $node)
+{
+	if($node['type'] == 'slave')
+	{
+		$graph->addEdge(array('Slave '.$node['id'] => 'Master'), array('color' => 'blue'));
+
+	}
+}
 
 $graph->addEdge(array('Master' => 'Router'), array('color' => 'green'));
 
