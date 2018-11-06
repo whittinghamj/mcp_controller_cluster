d678a407d8b8c69459e5817b359fb6cb69c85334
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-06 06:14:36 +0000

    no message

--- console.php
+++ console.php
@@ -832,7 +832,7 @@ if($task == "controller_checkin")
 
 		$post_data_json						= json_encode($post_data);
 
-		print_r($post_data_json, true);
+		print_r($post_data, true);
 
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $api_url."/api/?key=".$config['api_key']."&c=controller_checkin");
