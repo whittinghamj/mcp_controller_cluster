63605b2d64df11ebeff38100ea1d51d9ab46228a
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-07 04:49:06 +0000

    no message

--- test.php
+++ test.php
@@ -1,71 +1,36 @@
 <?php
 
-class MyTask extends Threaded 
+class SomeThreadedClass extends Thread
 {
-    private $m_inputs;
-    private $m_outputs;
-
-
-    public function __construct(array $inputs)
+    private $tID;
+    public $data;
+ 
+    public function __construct(int $tID)
     {
-        $this->m_inputs = $inputs;
-        $this->m_outputs = new Threaded(); // we will store the results in here.
+        $this->tID = $tID;
+        $this->data = $tID . ":" . date('H:i:s');
     }
-
-
-    public function run() 
+ 
+    public function run()
     {
-        foreach ($this->m_inputs as $input)
-        {
-            // casting the array to an array is not a mistake
-            // but actually super important for this to work
-            // https://github.com/krakjoe/pthreads/issues/813#issuecomment-361955116
-            $this->m_outputs[] = (array)array(
-                'property1' => $input * 2,
-                'property2' => ($input + 2),
-            );
-        }
+        echo $this->tID . " started.\n";
+        sleep($this->tID);
+        echo $this->tID . " ended. " . date('H:i:s') . "\n";
     }
-
-    # Accessors
-    public function getResults() { return $this->m_outputs; }
+}
+ 
+$threads = [];
+ 
+for ($i = 1; $i < 5; $i++) {
+    $threads[$i] = new SomeThreadedClass($i);
+    $threads[$i]->start();          // start the job on the background
+}
+ 
+for ($i = 1; $i < 5; $i++) {
+    $threads[$i]->join();           // wait until job is finished, 
+    echo $threads[$i]->data . "\n"; // then we can access the data
 }
 
+$threads[$i]->start();
 
-
-function main()
-{
-    $inputs = range(0,10000);
-    $numInputsPerTask = 20;
-    $inputGroups = array_chunk($inputs, $numInputsPerTask);
-    $numCpus = 4; // I would nomrally dynamically fetch this and sometimes large (e.g. aws C5 instances)
-    $numTasks = count($inputGroups);
-    $numThreads = min($numTasks, $numCpus); // don't need to spawn more threads than tasks.
-    $pool = new Pool($numThreads);
-    $tasks = array(); // collection to hold all the tasks to get the results from afterwards.
-
-    foreach ($inputGroups as $inputsForTask)
-    {
-        $task = new MyTask($inputsForTask);
-        $tasks[] = $task;
-        $pool->submit($task);
-    }
-
-
-    while ($pool->collect());
-
-    # We could submit more stuff here, the Pool is still waiting for work to be submitted.
-
-    $pool->shutdown();
-
-    # All tasks should have been completed at this point. Get the results!
-    $results = array();
-    foreach ($tasks as $task)
-    {
-        $results[] = $task->getResults();
-    }
-
-    print "results: " . print_r($results, true);
-}
-
-main();
+$threads[$i]->join();
