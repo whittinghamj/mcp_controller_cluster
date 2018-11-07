a4a61afe67be07315ce72080109aba06faf15857
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-07 00:58:24 +0000

    no message

--- setup.php
+++ setup.php
@@ -7,23 +7,31 @@ ini_set('error_reporting', E_ALL);
 
 include('/mcp_cluster/functions.php');
 
-// $task = $argv[1];
 
-// sanity checks
-if(file_exists("/etc/mcp/global_vars.php"))
+$data 					= get_system_stats();
+
+if($data['node_type'] == 'slave')
 {
-	console_output("Cluster is already configured.");
-	die();
+	if(file_exists("/etc/mcp/global_vars.php"))
+	{
+		console_output("Cluster is already configured.");
+		die();
+	}
+
+	$master_ip_address 		= gethostbyname('cluster-master');
+
+	$config_data			= file_get_contents("http://".$master_ip_address.":1372/web_api.php?c=cluster_configuration");
+	$config 				= json_decode($config_data, true);
+
+	exec("echo '<?php' > /etc/mcp/global_vars.php");
+	exec("echo '' >> /etc/mcp/global_vars.php");
+	exec("echo '' >> /etc/mcp/global_vars.php");
+	exec("echo $config['api_key'] = ".$config['api_key']."; >> /etc/mcp/global_vars.php");
+	exec("echo '' >> /etc/mcp/global_vars.php");
+	exec("echo $config['master'] = ".$config['master_ip_address']."; >> /etc/mcp/global_vars.php");
+
+	console_output("MCP Cluster is now configured.");
+	// fire_led('success');
+}else{
+	console_output("MCP Cluster Master cannot run this file.");
 }
-
-$master_ip_address 		= gethostbyname('cluster-master');
-
-$config_data			= file_get_contents("http://".$master_ip_address.":1372/web_api.php?c=cluster_configuration");
-$config 				= json_decode($config_data, true);
-
-$config_file '<?php $arr = ' . var_export($config, true) . ';';
-
-file_put_contents('/etc/mcp/global_vars.php', $config_file);
-
-console_output("MCP Cluster is now configured.");
-// fire_led('success');
