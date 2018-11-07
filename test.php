11fe054c36129c023edd33fdef0b8cc45895512e
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-07 04:58:01 +0000

    no message

--- test.php
+++ test.php
@@ -1,36 +1,40 @@
 <?php
-
-class SomeThreadedClass extends Thread
-{
-    private $tID;
-    public $data;
- 
-    public function __construct(int $tID)
-    {
-        $this->tID = $tID;
-        $this->data = $tID . ":" . date('H:i:s');
-    }
- 
-    public function run()
-    {
-        echo $this->tID . " started.\n";
-        sleep($this->tID);
-        echo $this->tID . " ended. " . date('H:i:s') . "\n";
-    }
+/**
+ * @file
+ * Basic demonstration of how to do parallel threads in PHP.
+ */
+// This array of "tasks" could be anything. For demonstration purposes
+// these are just strings, but they could be a callback, class or
+// include file (hell, even code-as-a-string to pass to eval()).
+$tasks = [
+  "fetch_remote_data",
+  "post_async_updates",
+  "clear_caches",
+  "notify_admin",
+];
+// This loop creates a new fork for each of the items in $tasks.
+foreach ($tasks as $task) {
+  $pid = pcntl_fork();
+  if ($pid == -1) {
+    exit("Error forking...\n");
+  }
+  else if ($pid == 0) {
+    execute_task($task);
+    exit();
+  }
 }
- 
-$threads = [];
- 
-for ($i = 1; $i < 5; $i++) {
-    $threads[$i] = new SomeThreadedClass($i);
-    $threads[$i]->start();          // start the job on the background
+// This while loop holds the parent process until all the child threads
+// are complete - at which point the script continues to execute.
+while(pcntl_waitpid(0, $status) != -1);
+// You could have more code here.
+echo "Do stuff after all parallel execution is complete.\n";
+/**
+ * Helper method to execute a task.
+ */
+function execute_task($task_id) {
+  echo "Starting task: ${task_id}\n";
+  // Simulate doing actual work with sleep().
+  $execution_time = rand(5, 10);
+  sleep($execution_time);
+  echo "Completed task: ${task_id}. Took ${execution_time} seconds.\n";
 }
- 
-for ($i = 1; $i < 5; $i++) {
-    $threads[$i]->join();           // wait until job is finished, 
-    echo $threads[$i]->data . "\n"; // then we can access the data
-}
-
-$threads[$i]->start();
-
-$threads[$i]->join();
