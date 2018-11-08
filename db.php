adaeedb502a218f516a5b422019a987c3ee6aa8b
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-08 07:40:43 +0000

    no message

--- db.php
+++ db.php
@@ -4,12 +4,23 @@
 $config_file		= @file_get_contents('/etc/mcp/global_vars.php');
 $config 			= json_decode($config_file, true);
 
+// check is this node is a master or a slave
+$data['hostname']               = exec('cat /etc/hostname');
+if($data['hostname'] == 'cluster-master')
+{
+    $data['node_type']          = 'master';
+    $database['hostname']		= 'localhost';
+}else{
+    $data['node_type']          = 'slave';
+    $database['hostname']		= $config['master'];
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // mysql settings
 $database['username']	= "root";
 $database['password']	= "admin1372";
 $database['database']	= "mcp_cluster";
-$database['hostname']	= $config['master'];
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // mysql connection
 
