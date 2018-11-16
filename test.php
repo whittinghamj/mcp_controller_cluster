6afafeeafcc03b468ecad1bc1d3b9d0eb248866d
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-16 00:19:16 +0000

    no message

--- test.php
+++ test.php
@@ -37,25 +37,24 @@ foreach(range(0, 58) as $time)
 
 	if($data['cpu_load'] >= 0 && $data['cpu_load'] < 10)
 	{
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 0 255 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 0 255 0', 1);
 	}elseif($data['cpu_load'] >=11 && $data['cpu_load'] < 20){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 56 255 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 56 255 0', 1);
 	}elseif($data['cpu_load'] >=21 && $data['cpu_load'] < 30){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 113 255 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 113 255 0', 1);
 	}elseif($data['cpu_load'] >=31 && $data['cpu_load'] < 40){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 170 255 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 170 255 0', 1);
 	}elseif($data['cpu_load'] >=41 && $data['cpu_load'] < 50){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 226 255 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 226 255 0', 1);
 	}elseif($data['cpu_load'] >=51 && $data['cpu_load'] < 60){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 226 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 226 0', 1);
 	}elseif($data['cpu_load'] >=61 && $data['cpu_load'] < 70){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 170 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 170 0', 1);
 	}elseif($data['cpu_load'] >=71 && $data['cpu_load'] < 80){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 113 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 113 0', 1);
 	}elseif($data['cpu_load'] >=81 && $data['cpu_load'] < 90){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 56 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 56 0', 1);
 	}elseif($data['cpu_load'] >=91){
-		exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 0 0');
+		exec_timeout('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 0 0', 1);
 	}
-	sleep(1);
 }
