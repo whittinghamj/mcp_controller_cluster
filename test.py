75348b211e5d8911718894b29538916265695502
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-16 01:25:22 +0000

    no message

--- test.py
+++ test.py
@@ -23,28 +23,28 @@ while True:
 
     pixels = random.sample(range(blinkt.NUM_PIXELS), random.randint(1, 5))
     for i in range(blinkt.NUM_PIXELS):
-        print i
+        # print i
         if i in pixels:
             if cpu < 10:
-                blinkt.set_all(i, 0,255,0)         # Green
+                blinkt.set_pixel(i, 0,255,0)         # Green
             elif (cpu > 11) and (cpu < 20):
-                blinkt.set_all(i, 56,255,0)
+                blinkt.set_pixel(i, 56,255,0)
             elif (cpu > 21) and (cpu < 30): # Lime
-                blinkt.set_all(i, 113,255,0)
+                blinkt.set_pixel(i, 113,255,0)
             elif (cpu > 31) and (cpu < 40):
-                blinkt.set_all(i, 170,255,0)
+                blinkt.set_pixel(i, 170,255,0)
             elif (cpu > 41) and (cpu < 50): # Yellow
-                blinkt.set_all(i, 226,255,0)
+                blinkt.set_pixel(i, 226,255,0)
             elif (cpu > 51) and (cpu < 60):
-                blinkt.set_all(i, 255,226,0)
+                blinkt.set_pixel(i, 255,226,0)
             elif (cpu > 61) and (cpu < 70): # Orange
-                blinkt.set_all(i, 255,170,0)
+                blinkt.set_pixel(i, 255,170,0)
             elif (cpu > 71) and (cpu < 80):
-                blinkt.set_all(i, 255,113,0)
+                blinkt.set_pixel(i, 255,113,0)
             elif (cpu > 81) and (cpu < 90):
-                blinkt.set_all(i, 255,56,0)
+                blinkt.set_pixel(i, 255,56,0)
             else:
-                blinkt.set_all(i, 255,0,0)
+                blinkt.set_pixel(i, 255,0,0)
 
         else:
             blinkt.set_pixel(i, 0, 0, 0)
