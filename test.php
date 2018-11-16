e57763ab9917a91661e68f7b409098ba169482cb
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-16 00:55:47 +0000

    no message

--- test.php
+++ test.php
@@ -35,37 +35,40 @@ foreach(range(0, 58) as $time)
 
 	console_output("CPU Load: ".$data['cpu_load']);
 
-	$descriptorspec = array(
-		0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-		1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-		2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
-	);
-
-	if($data['cpu_load'] >= 0 && $data['cpu_load'] < 10)
-	{
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 0 255 0');
-	}elseif($data['cpu_load'] >=11 && $data['cpu_load'] < 20){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 56 255 0');
-	}elseif($data['cpu_load'] >=21 && $data['cpu_load'] < 30){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 113 255 0');
-	}elseif($data['cpu_load'] >=31 && $data['cpu_load'] < 40){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 170 255 0');
-	}elseif($data['cpu_load'] >=41 && $data['cpu_load'] < 50){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 226 255 0');
-	}elseif($data['cpu_load'] >=51 && $data['cpu_load'] < 60){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 226 0');
-	}elseif($data['cpu_load'] >=61 && $data['cpu_load'] < 70){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 170 0');
-	}elseif($data['cpu_load'] >=71 && $data['cpu_load'] < 80){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 113 0');
-	}elseif($data['cpu_load'] >=81 && $data['cpu_load'] < 90){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 56 0');
-	}elseif($data['cpu_load'] >=91){
-		$proc = exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 0 0');
+	$pid = pcntl_fork();
+	if($pid == -1){
+	    die('could not fork');
+	}elseif($pid){
+	    // we are the parent
+	    pcntl_wait($status); //Protect against Zombie children
+	}else{
+		if($data['cpu_load'] >= 0 && $data['cpu_load'] < 10)
+		{
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 0 255 0');
+		}elseif($data['cpu_load'] >=11 && $data['cpu_load'] < 20){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 56 255 0');
+		}elseif($data['cpu_load'] >=21 && $data['cpu_load'] < 30){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 113 255 0');
+		}elseif($data['cpu_load'] >=31 && $data['cpu_load'] < 40){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 170 255 0');
+		}elseif($data['cpu_load'] >=41 && $data['cpu_load'] < 50){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 226 255 0');
+		}elseif($data['cpu_load'] >=51 && $data['cpu_load'] < 60){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 226 0');
+		}elseif($data['cpu_load'] >=61 && $data['cpu_load'] < 70){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 170 0');
+		}elseif($data['cpu_load'] >=71 && $data['cpu_load'] < 80){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 113 0');
+		}elseif($data['cpu_load'] >=81 && $data['cpu_load'] < 90){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 56 0');
+		}elseif($data['cpu_load'] >=91){
+			exec('python /mcp_cluster/cluster_workload_with_cpu_load.py 255 0 0');
+		}
 	}
 
+
 	// run the non-blocking process
-    stream_set_blocking($proc, 0);
+    // stream_set_blocking($proc, 0);
 
     // sleep for 1 second
 	sleep(1);
